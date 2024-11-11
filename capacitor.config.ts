7d15f13d80cda47a6bef37ae383620f4ad2117a5
Full Name <theofficialwebsiteguys@gmail.com>
2024-11-11 15:42:56 -0500

    init ionic app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'flower-power-dispensers',
+  webDir: 'www'
+};
+
+export default config;
