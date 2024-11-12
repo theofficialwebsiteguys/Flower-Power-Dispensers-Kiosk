e3a0f85e132294a8a3380a38fa75fdcfb8483839
Full Name <theofficialwebsiteguys@gmail.com>
2024-11-12 12:19:05 -0500

    FPD-28

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'flower-power-dispensers',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0, // Disables Capacitor's default splash screen to use custom Angular component
+    },
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.2s ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  constructor() {}
+  showSplashScreen = true;
+
+
+  onCloseSplash() {
+    // Delay hiding the splash screen to allow the fade-out animation to complete
+      this.showSplashScreen = false;
+      console.log("Splash screen closed, showSplashScreen:", this.showSplashScreen);
+  }
 }
