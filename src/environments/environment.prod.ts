9391e5c8c15c4f05ac21da96decf38c98218d9d1
Matt Pero <mattpero22@gmail.com>
2024-12-20 12:18:16 -0500

    resolve issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://dispensary-api-ac9613fa4c11.herokuapp.com/api',
-<<<<<<< HEAD
-  venue_id: 'df759ba5e7360809',
-=======
   venueId: '836944c298d40676',
->>>>>>> a9dd8b5294b6611dbfea3ff4219dca9f795b8e15
   flower_power_api_key: '564ee591-42df-46d9-91c0-ff3507e316ec'
 };
