a26fe27d7f9e5b75da2b5f5f2ce1e201e496df73
Full Name <theofficialwebsiteguys@gmail.com>
2025-02-18 12:42:56 -0500

    Deploy Ionic PWA to GitHub Pages

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://dispensary-api-ac9613fa4c11.herokuapp.com/api',
+  venueId: 'df759ba5e7360809',
+  flower_power_api_key: '564ee591-42df-46d9-91c0-ff3507e316ec',
+  aeropay_api_key: '016c0534-ff89-4626-b8a4-fdbe7fbe1f08',
+  aeropay_api_secret: '13322af3-39fb-462d-865c-9f238b248abd',
+  aeropay_url: "https://api.aeropay.com",
+  aeropay_merchant_id: 1689,
+  db_api_key: 'c69203dbdeaf88d28f3bfa28afeaff32965744f3d3ae6321b9eff6d198b1edfb'
+};
